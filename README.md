64e4485986c4cfb302f2125886cadac16dfcd3ff
Simon Engledew <simon@engledew.com>
2011-08-10 10:54:40 +0100

    changed example, added getitem

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ import sshim
 def hello_world(script):
     script >> 'Please enter your name: '
     script << '(?P<name>.*)'
+    print script['name'], 'just connected'
     script >> 'Hello %(name)s!\r\n'
 
 server = sshim.Server(hello_world, port=3000)
--- sshim.py
+++ sshim.py
@@ -113,6 +113,12 @@ class Script(object):
         self.fileobj = fileobj
         self.values = {}
 
+    def __getitem__(self, key):
+        return self.values[key]
+
+    def __setitem__(self, key, value):
+        self.values[key] = value
+
     def __rshift__(self, line):
         self.fileobj.write(line % self.values)
 
