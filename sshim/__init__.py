83e6ad2dc6ab587ae3da010ee55a785328fc16a5
Simon Engledew <simon@engledew.com>
2011-10-13 10:31:23 +0100

    still working on how I want the documenation to look. just uploaded the sphinx output to pypi

--- sshim/__init__.py
+++ sshim/__init__.py
@@ -1,7 +1,7 @@
 # encoding: utf8
 
 """
-SSHim is a library for testing and debugging SSH automation clients. The aim is to provide a scriptable SSH server which can be made to behave like any SSH-enabled device.
+SSHim (https://github.com/simon-engledew/sshim) is a library for testing and debugging SSH automation clients. The aim is to provide a scriptable SSH server which can be made to behave like any SSH-enabled device.
 
 Currently SSHim does just enough to fire up an SSH server using Paramiko and read and write values. Eventually it could be expanded to support additional channel types, scripted tunneling and more.
 
