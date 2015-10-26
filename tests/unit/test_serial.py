569e4df5a2906b6aaec9163a9b9e55635bb35007
Simon Engledew <simon.engledew@oup.com>
2015-10-26 14:46:24 +0000

    Add a fileobj.close to try and fix an intermittent test failure in Python 2.7.9

--- tests/unit/test_serial.py
+++ tests/unit/test_serial.py
@@ -10,6 +10,7 @@ class TestMultipleServers(unittest.TestCase):
     def assert_success(self, server):
         with connect(server) as fileobj:
             self.assertEqual(fileobj.readline(), 'success\r\n')
+            fileobj.close()
 
     def test_one_after_another(self):
         with sshim.Server(success, port=3000) as server:
