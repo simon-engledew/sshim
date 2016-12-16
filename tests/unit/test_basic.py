c5e43f032ae782295805b64c46918924276fd600
Britany Meadows <britany.meadows@adtran.com>
2016-12-16 10:03:07 -0600

    add support for ipv6

--- tests/unit/test_basic.py
+++ tests/unit/test_basic.py
@@ -17,3 +17,16 @@ class TestBasic(unittest.TestCase):
                 fileobj.flush()
                 assert fileobj.readline() == 'test_echo\r\n'
                 assert fileobj.readline() == 'return test_echo\r\n'
+
+    def test_echo_ipv6(self):
+        def echo(script):
+            groups = script.expect(re.compile('(?P<value>.*)')).groupdict()
+            assert groups['value'] == 'test_echo'
+            script.writeline('return %(value)s' % groups)
+
+        with sshim.Server(echo, address='::1', port=10008) as server:
+            with connect(server) as fileobj:
+                fileobj.write('test_echo\n')
+                fileobj.flush()
+                assert fileobj.readline() == 'test_echo\r\n'
+                assert fileobj.readline() == 'return test_echo\r\n'
